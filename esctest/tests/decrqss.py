c9292bbe20dddb4f51fdfadc88ff7a48d7466349
George Nachman <gnachman@gmail.com>
2021-10-06 10:25:04 -0700

    Update DECRQSS. Add DECSLPP to it, remove knownBug annotations for iTerm2

--- esctest/tests/decrqss.py
+++ esctest/tests/decrqss.py
@@ -13,7 +13,7 @@ class DECRQSSTests(object):
     AssertEQ(result, '1$r1"q')
 
   @vtLevel(4)
-  @knownBug(terminal="iTerm2", reason="Not implemented.")
+  @knownBug(terminal="iTerm2", reason="VT400 not implemented yet (will report a lower value).")
   def test_DECRQSS_DECSCL(self):
     esccmd.DECSCL(65, 1)
     esccmd.DECRQSS('"p')
@@ -21,7 +21,6 @@ class DECRQSSTests(object):
     AssertEQ(result, '1$r6' + str(escargs.args.max_vt_level) + ';1"p')
 
   @vtLevel(4)
-  @knownBug(terminal="iTerm2", reason="Not implemented.")
   def test_DECRQSS_DECSTBM(self):
     esccmd.DECSTBM(5, 6)
     esccmd.DECRQSS("r")
@@ -29,7 +28,6 @@ class DECRQSSTests(object):
     AssertEQ(result, "1$r5;6r")
 
   @vtLevel(4)
-  @knownBug(terminal="iTerm2", reason="Not implemented.")
   def test_DECRQSS_SGR(self):
     esccmd.SGR(1)
     esccmd.DECRQSS("m")
@@ -37,7 +35,6 @@ class DECRQSSTests(object):
     AssertEQ(result, "1$r0;1m")
 
   @vtLevel(4)
-  @knownBug(terminal="iTerm2", reason="Not implemented.")
   def test_DECRQSS_DECSCUSR(self):
     esccmd.DECSCUSR(4)
     esccmd.DECRQSS(" q")
@@ -45,7 +42,6 @@ class DECRQSSTests(object):
     AssertEQ(result, "1$r4 q")
 
   @vtLevel(4)
-  @knownBug(terminal="iTerm2", reason="Not implemented.")
   def test_DECRQSS_DECSLRM(self):
     """Note: not in xcode docs, but supported."""
     esccmd.DECSET(esccmd.DECLRMM)
@@ -53,3 +49,10 @@ class DECRQSSTests(object):
     esccmd.DECRQSS("s")
     result = escio.ReadDCS()
     AssertEQ(result, "1$r3;4s")
+
+  @vtLevel(4)
+  def test_DECRQSS_DECSLPP(self):
+    esccmd.XTERM_WINOPS(27)
+    esccmd.DECRQSS("t")
+    result = escio.ReadDCS()
+    AssertEQ(result, "1$r27t")
